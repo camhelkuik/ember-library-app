9750f8717957038c89331495ef00a43628aa8bee
Cameron Helkuik <cameron@travefy.com>
2017-04-18 21:06:19 -0500

    option 2, moved all the code, isValid is not working..

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,21 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    emailIsValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-    textIsValid: Ember.computed.gte('message.length', 5),
-    isValid: Ember.computed.and('emailIsValid', 'textIsValid'),
-
-    actions: {
-         sendMessage() {
-             const email = this.get('emailAddress');
-             const message = this.get('message');
-
-             const newContact = this.store.createRecord('contact', {email:email, message:message});
-             newContact.save().then((response) => {
-                this.set('responseMessage', `We got your message with id: ${response.get('id')}. We'll be in touch soon`);
-                this.set('emailAddress', '');
-                this.set('message', '');
-             });
-        }
-    }
+   
 });
--- app/models/contact.js
+++ app/models/contact.js
@@ -2,5 +2,11 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  message: DS.attr('string')
+  message: DS.attr('string'),
+
+  emailIsValid: Ember.computed.match('email', /^.+@.+\..+$/),
+  textIsValid: Ember.computed.gte('message.length', 5),
+  isValid: Ember.computed.and('emailIsValid', 'textIsValid'),
+  isNotValid: Ember.computed.not('isValid')
+
 });
