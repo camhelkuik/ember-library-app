4add1cb7af0a666d60f0b01b43884091bce085c4
Cameron Helkuik <cameron@travefy.com>
2017-04-21 20:26:09 -0500

    lesson 6 got complex, need to go back and work through what we did

---
+++ app/routes/admin/seeder.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+    model() {
+        return Ember.RSVP.hash({
+            libraries: this.store.findAll('library'),
+            books: this.store.findAll('book'),
+            authors: this.store.findAll('author')
+        })
+    }
+});
